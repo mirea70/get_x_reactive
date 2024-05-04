314d64cde49bd8b5281f95aa8d02e3f3f9b9baa9
GrowMyself <deae670@daum.net>
2024-05-04 14:16:05 +0900

    study: get_x Reactive State manager

---
+++ lib/controller.dart
@@ -0,0 +1,13 @@
+import 'package:get/get.dart';
+import 'model.dart';
+
+class Controller extends GetxController {
+  final person = Person().obs;
+
+  void updateInfo() {
+    person.update((val) {
+      val?.age++;
+      val?.name = 'Coding Chef';
+    });
+  }
+}
